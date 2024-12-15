1c98130b03eeb4aebbf1c060aa0518f2abea47e8
yryo1005 <33744568+yryo1005@users.noreply.github.com>
2024-12-15 17:53:46 +0900

    0

---
+++ Neural_Joking_Machine_v1/train.py
@@ -0,0 +1,315 @@
+import argparse
+from utils import *
+
+# コマンドライン引数の処理
+parser = argparse.ArgumentParser(description="設定用プログラム")
+
+# 実験番号
+parser.add_argument("--experience_number", type = str, help = "実験番号")
+# PCによって変更する
+parser.add_argument("--num_workers", type = int, default = 16, help = "データローダが使用するCPUのスレッド数(GPUの総スレッド数の8割が推奨)")
+
+# データセットが既に存在する場合に，再度作り直すか
+parser.add_argument("--reset_data", action = "store_true", help = "データセットを再作成するか")
+# 現実写真以外を使用するか
+parser.add_argument("--use_unreal_image", action = "store_true", help = "現実写真以外を使用する")
+# 文字を含む画像を使用するか
+parser.add_argument("--use_word_image", action = "store_true", help = "文字を含む画像を使用する")
+# 固有名詞を含む大喜利を使用するか
+parser.add_argument("--use_unique_noun_boke", action = "store_true", help = "固有名詞を含む大喜利を使用する")
+
+# 大喜利の最小の星の数
+parser.add_argument("--min_star", type = int, default = 0, help = "大喜利の最小の星の数")
+# 単語の最小出現回数
+parser.add_argument("--min_apper_word", type = int, default = 32, help = "単語の最小出現回数")
+# 大喜利の最小単語数
+parser.add_argument("--min_sentence_length", type = int, default = 4, help = "大喜利の最小単語数")
+# 大喜利の最大単語数
+parser.add_argument("--max_sentence_length", type = int, default = 31, help = "大喜利の最大単語数")
+
+args = parser.parse_args()
+
+EXPERIENCE_NUMBER = args.experience_number
+
+# PCによって変更する
+NUM_WORKERS = args.num_workers
+# データセットが既に存在する場合に，再度作り直すか
+RESET_DATA = args.reset_data
+
+# 現実写真以外を使用するか
+USE_UNREAL_IMAGE = args.use_unreal_image
+# 文字を含む画像を使用するか
+USE_WORD_IMAGE = args.use_word_image
+# 固有名詞を含む大喜利を使用するか
+USE_UNIQUE_NOUN_BOKE = args.use_unique_noun_boke
+
+# 大喜利の最小の星の数
+MIN_STAR = args.min_star
+# 単語の最小出現回数
+MIN_APPER_WORD = args.min_apper_word
+# 大喜利の最小単語数
+MIN_SENTENCE_LENGTH = args.min_sentence_length
+# 大喜利の最大単語数
+MAX_SENTENCE_LENGTH = args.max_sentence_length
+
+RESULT_DIR = f"../../results/Neural_Joking_Machine/{EXPERIENCE_NUMBER}/"
+if not os.path.exists("../../results/Neural_Joking_Machine/"):
+    os.mkdir("../../results/Neural_Joking_Machine/")
+if not os.path.exists(RESULT_DIR):
+    os.mkdir(RESULT_DIR)
+print(f"result directory: {RESULT_DIR}")
+
+# データセットの作成
+if not os.path.exists(f"{RESULT_DIR}index_to_word.json") or RESET_DATA:
+    # tokenizer
+    with open('Japanese_BPEEncoder_V2/ja-swe32kfix.txt') as f:
+        bpe = f.read().split('\n')
+
+    with open('Japanese_BPEEncoder_V2/emoji.json') as f:
+        emoji = json.loads(f.read())
+
+    tokenizer = SWEEncoder_ja(bpe, emoji)
+
+    tmp = list()
+    word_count_dict = dict()
+
+    for JP in tqdm(os.listdir(DATA_DIR)):
+        
+        N = int(JP.split(".")[0])
+
+        with open(f"{DATA_DIR}{JP}", "r") as f:
+            a = json.load(f)
+        
+        image_information = a["image_information"]
+        is_photographic_probability = image_information["is_photographic_probability"]
+        ocr = image_information["ocr"]
+
+        # 現実写真以外を除去
+        if not USE_UNREAL_IMAGE:
+            if is_photographic_probability < 0.8: continue
+            
+        # 文字のある画像を除去
+        if not USE_WORD_IMAGE:
+            if len(ocr) != 0: continue
+        
+        bokes = a["bokes"]
+
+        for B in bokes:
+            # 星が既定の数以下の大喜利を除去
+            if B["star"] < MIN_STAR:
+                continue
+
+            # 固有名詞を含む大喜利を除去
+            if not USE_UNIQUE_NOUN_BOKE:
+                if len(B["unique_nouns"]) != 0: continue
+
+            tokenized_boke = tokenizer.encode(B["boke"])
+            # 単語数が既定の数でない大喜利を除去
+            if not MIN_SENTENCE_LENGTH <= len(tokenized_boke) < MAX_SENTENCE_LENGTH:
+                continue
+
+            for W in tokenized_boke:
+                try:
+                    word_count_dict[W] += 1
+                except:
+                    word_count_dict[W] = 1
+            
+            tmp.append({
+                "image_number": N,
+                "tokenized_boke": tokenized_boke
+            })
+
+    # 単語の最小出現回数を満たさない大喜利を除去
+    boke_datas = list()
+    words = list()
+
+    for D in tqdm(tmp):
+        flag = False
+        for W in D["tokenized_boke"]:
+            if word_count_dict[W] < MIN_APPER_WORD:
+                flag = True
+                break
+        if flag: 
+            continue
+        
+        boke_datas.append({
+            "image_number": D["image_number"],
+            "tokenized_boke": D["tokenized_boke"]
+        })
+        words += D["tokenized_boke"]
+    words = set(words)
+    image_numbers = list(set([D["image_number"] for D in boke_datas]))
+    del tmp
+
+    # tokenize
+    index_to_index = dict()
+
+    c = 3
+    for D in tqdm(boke_datas):
+        tmp = list()
+        for W in D["tokenized_boke"]:
+            try:
+                index_to_index[W]
+            except:
+                index_to_index[W] = c
+                c += 1
+            tmp.append(index_to_index[W])
+        D["tokenized_boke"] = [1] + tmp + [2]
+
+    index_to_word = {
+        V: tokenizer.decode([K]) for K, V in index_to_index.items()
+    }
+    index_to_word[0] = "<PAD>"
+    index_to_word[1] = "<START>"
+    index_to_word[2] = "<END>"
+
+    #
+    train_boke_datas, test_boke_datas = train_test_split(boke_datas, test_size = 0.01)
+
+    with open(f"{RESULT_DIR}train_boke_datas.json", "w") as f:
+        json.dump(train_boke_datas, f)
+    with open(f"{RESULT_DIR}test_boke_datas.json", "w") as f:
+        json.dump(test_boke_datas, f)
+    with open(f"{RESULT_DIR}index_to_word.json", "w") as f:
+        json.dump(index_to_word, f)
+
+else:
+    with open(f"{RESULT_DIR}train_boke_datas.json", "r") as f:
+        train_boke_datas = json.load(f)
+    with open(f"{RESULT_DIR}test_boke_datas.json", "r") as f:
+        test_boke_datas = json.load(f)
+    with open(f"{RESULT_DIR}index_to_word.json", "r") as f:
+        index_to_word = json.load(f)
+
+    image_numbers = [D["image_number"] for D in train_boke_datas] + [D["image_number"] for D in test_boke_datas]
+    image_numbers = list(set(image_numbers))
+
+print(f"学習に用いる大喜利の数: {len(train_boke_datas)}\n", 
+      f"検証に用いる大喜利の数: {len(test_boke_datas)}\n",
+      f"使用する画像の数: {len(image_numbers)}\n",
+      f"単語の種類: {len(index_to_word)}")
+
+# 画像を特徴量に変換する
+tmp = list()
+for IN in tqdm(image_numbers):
+    if os.path.exists(f"{IMAGE_FEATURE_DIR}{IN}.npy"):
+        continue
+    tmp.append(IN)
+
+if len(tmp) != 0:
+    image_dataloader = make_image_dataloader(tmp, num_workers = NUM_WORKERS)
+
+    # resnet152
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = models.resnet152(pretrained = True)
+    model = torch.nn.Sequential(*list(model.children())[:-1] + [nn.Flatten()])
+    model = model.to(device)
+    model.eval()
+
+    for Is, INs in tqdm(image_dataloader):
+        Is = Is.to(device)
+        features = model(Is).detach().cpu().numpy()
+
+        for F, IN in zip(features, INs):
+            np.save(f"{IMAGE_FEATURE_DIR}{IN}", F)
+
+train_dataloader = make_dataloader(train_boke_datas, max_sentence_length = MAX_SENTENCE_LENGTH, num_workers = NUM_WORKERS)
+test_dataloader = make_dataloader(test_boke_datas, max_sentence_length = MAX_SENTENCE_LENGTH, num_workers = NUM_WORKERS)
+
+# モデルの学習
+train_loss_history = []
+train_accuracy_history = []
+test_loss_history = []
+test_accuracy_history = []
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = BokeGeneratorModel(num_word = len(index_to_word), 
+                           image_feature_dim = 2048, 
+                           sentence_length = MAX_SENTENCE_LENGTH + 1, 
+                           embedding_dim = 512)
+model.to(device)
+optimizer = optim.AdamW(model.parameters(), lr = LEARNING_RATO)
+
+for epoch in range(EPOCH):
+
+    # train
+    train_loss_obj = 0.0
+    train_accuracy_obj = 0.0
+    model.train()
+    pb = tqdm(train_dataloader, desc = f"Epoch {epoch+1}/{EPOCH}")
+    
+    for image_features, sentences, teacher_signals in pb:
+        image_features = image_features.to(device)
+        sentences = sentences.to(device)
+        teacher_signals = teacher_signals.to(device)
+        
+        loss, accuracy = train_step(model, optimizer, (image_features, sentences), teacher_signals)
+        train_loss_obj += loss
+        train_accuracy_obj += accuracy
+        pb.set_postfix({"train_loss": train_loss_obj / (pb.n + 1), "train_accuracy": train_accuracy_obj / (pb.n + 1)})
+
+    train_loss = train_loss_obj / len(train_dataloader)
+    train_accuracy = train_accuracy_obj / len(train_dataloader)
+
+    # test
+    test_loss_obj = 0.0
+    test_accuracy_obj = 0.0
+    model.eval()
+    pb = tqdm(test_dataloader, desc = "Evaluating")
+
+    for image_features, sentences, teacher_signals in pb:
+        image_features = image_features.to(device)
+        sentences = sentences.to(device)
+        teacher_signals = teacher_signals.to(device)
+
+        loss, accuracy = evaluate(model, (image_features, sentences), teacher_signals)
+        test_loss_obj += loss
+        test_accuracy_obj += accuracy
+        pb.set_postfix({"test_loss": test_loss_obj / (pb.n + 1), "test_accuracy": test_accuracy_obj / (pb.n + 1)})
+
+    test_loss = test_loss_obj / len(test_dataloader)
+    test_accuracy = test_accuracy_obj / len(test_dataloader)
+
+    print(f"Epoch: {epoch+1}/{EPOCH}, "
+          f"Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, "
+          f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
+    print("-" * 25)
+
+    train_loss_history.append(train_loss)
+    train_accuracy_history.append(train_accuracy)
+    test_loss_history.append(test_loss)
+    test_accuracy_history.append(test_accuracy)
+
+    # 学習精度を更新した場合、重みを保存
+    if max(train_accuracy_history) == train_accuracy:
+        torch.save(model.state_dict(), f"{RESULT_DIR}best_model_weights.pth")
+
+# 学習結果を保存
+with open(f"{RESULT_DIR}history.json", "w") as f:
+    json.dump({
+        "train_loss": train_loss_history,
+        "train_accuracy": train_accuracy_history,
+        "test_loss": test_loss_history,
+        "test_accuracy": test_accuracy_history
+    }, f)
+
+# 学習結果を描画
+fig = plt.figure(figsize = (10, 5))
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(train_loss_history, label = "train")
+ax.plot(test_loss_history, label = "test")
+ax.set_xlabel("epoch")
+ax.set_ylabel("loss")
+ax.legend()
+ax.grid()
+
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(train_accuracy_history, label = "train")
+ax.plot(test_accuracy_history, label = "test")
+ax.set_xlabel("epoch")
+ax.set_ylabel("accuracy")
+ax.legend()
+ax.grid()
+
+fig.savefig(f"{RESULT_DIR}history.png")
